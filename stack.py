a648bfb79d65dd0af36997e3e4b3d135aaea8e4d
AngelGaelGarciaRangel <angelgaelgarciarangel@gmail.com>
2025-04-03 21:11:26 -0600

    added stack

---
+++ stack.py
@@ -0,0 +1,20 @@
+class Stack:
+    def __init__(self):
+        self.stck = []
+    
+    def push(self, item):
+        self.stck.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.stck.pop()
+        else:
+            raise IndexError("pop from empty stack")
+    
+    def size(self):
+        return len(self.stck)
+
+    def is_empty(self):
+        return self.size() == 0
+    
+    
